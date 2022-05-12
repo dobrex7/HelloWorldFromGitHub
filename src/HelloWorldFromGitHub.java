685fd67d1c6fe18d3fd0955d83d1ce9836eec895
dobrex7 <dobispecijalec@gmail.com>
2022-05-12 19:51:34 +0200

    Push Pivska Steklenica

--- src/HelloWorldFromGitHub.java
+++ src/HelloWorldFromGitHub.java
@@ -14,6 +14,7 @@ public class HelloWorldFromGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		Steklenica ps = new PivskaSteklenica(500, "Whiskey", "Test", 5);
 	}
 
 }
---
+++ src/Steklenica.java
@@ -0,0 +1,110 @@
+//Avtor: Dobre Donev
+
+//Deklariramo javni razred
+public class Steklenica {
+	
+	
+		// Deklariramo lastnosti
+		// primitivni podatkovni tipi
+		private int kapaciteta; 			// v ml
+		private int kolicinaVsebine;		// v ml
+		private boolean jeOdprta;
+		// deklariramo objekt z imenom barva razreda String
+		private String vsebina;
+		
+
+
+		// deklariramo in inicializiramo statično lastnosti
+		/**
+		 * @return the vsebina
+		 */
+		public String getVsebina() {
+			return vsebina;
+		}
+
+		public static final String material = "steklo";
+		
+	
+		// deklariramo javni konstruktor
+		// ustvarimo polno zaprto steklenico
+		// parametri: količina vrste vsebine
+		// vrača : nov objekt razreda Steklenica
+		/**
+		 * @param k Količina
+		 * @param v Vsebina
+		 */
+		public Steklenica(int k, String v) {
+			
+			//Inicializiramo lastnosti
+			kapaciteta = k;
+			kolicinaVsebine = k;
+			jeOdprta = false;
+			vsebina = v;
+			
+			// Izpišemo dogodek
+			System.out.println("Ustavarjam steklenico prostornine " + kapaciteta + " ml z vsebino " + vsebina);
+		}
+		
+		// metoda, ki odpre steklenico
+		// argumenti: jih ni
+		// vrača: uspešnost odpiranja
+		/**
+		 * @return Vrne ali je steklenica uspešno zaprta,
+		 */
+		public boolean odpri() {
+			
+			if(jeOdprta)
+			{
+				System.out.println("Steklenica je ze odprta");
+				return false;
+			}
+			
+			System.out.println("Odpiram steklenico...");
+			jeOdprta = true;
+			return true;
+		}
+		
+		// deklariramo metodo, ki zmanjša količino vsebine
+		// parametri: količina, ki jo želimo izprazniti
+		// vrača: količina, ki je še ostala
+		public int izprazni (int k) {
+			
+			// če je steklenica odprta
+			
+			if(jeOdprta == true) {
+			
+			// če je v steklenici še dovolj vsebine
+			if (kolicinaVsebine >= k)
+			{
+			// od količine vsebine odštejemo količino za praznjenje
+			kolicinaVsebine = kolicinaVsebine -k;
+			System.out.println("Iz steklenice praznim " + k + " ml");
+			}
+			
+			//Če v steklenici ni dovolj vsebine
+			else {
+				
+				// Izpraznimo vse kar je v steklenici
+				kolicinaVsebine = 0;
+				System.out.println("Steklenica je prazna!");
+				}
+			
+			}
+			// Če je steklenica zaprta
+			else {
+				System.out.println("Steklenica je zaprta");
+			}
+			// vrnemo količino, ki je še ostsala
+			return kolicinaVsebine;
+		}
+		
+		// getter metoda , ki vrne količino vsebine
+		public int getKolicinaVsebine () {
+			return kolicinaVsebine;
+		}
+		
+		// getter metoda , ki vrne kapaciteto vsebine
+		public int getKapaciteta() {
+			return kapaciteta;
+		}
+}
