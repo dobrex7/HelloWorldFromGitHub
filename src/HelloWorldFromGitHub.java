e128f007e9b88c63143207d0bff9c549f5234966
dobrex7 <dobispecijalec@gmail.com>
2022-05-12 19:37:16 +0200

    Prvi testni push

---
+++ src/HelloWorldFromGitHub.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Dobre
+ *
+ */
+public class HelloWorldFromGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
